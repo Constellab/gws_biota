f439548e6b172e2eeda62871b53ae26572d89f43
Sarah Dandou <sarah.dandou@insa-lyon.fr>
2020-05-25 11:41:07 +0200

    Indexing GO_id and SBO_id
    Signed-off-by: Sarah Dandou <sarah.dandou@insa-lyon.fr>

--- src/hello/element.py
+++ src/hello/element.py
@@ -4,7 +4,7 @@ from gws.prism.app import App
 from gws.prism.model import Process
 from gws.prism.model import Model, ViewModel,ResourceViewModel, Resource, DbManager
 from gws.prism.controller import Controller
-from peewee import IntegerField
+from peewee import IntegerField, DateField, DateTimeField, CharField, ForeignKeyField
 
 ####################################################################################
 #
@@ -13,8 +13,8 @@ from peewee import IntegerField
 ####################################################################################
 
 class Element(Resource):
-    GO_id = 'Test'
-    SBO_id = 'Test'
+    GO_id = CharField(null=True, index=True)
+    SBO_id = CharField(null=True, index=True)
     _table_name = 'element'
     
     def __init__(self, *args, **kwargs):
