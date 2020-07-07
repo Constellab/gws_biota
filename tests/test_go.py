262946d315d8bb49f33d714ed2fe6303ff47fde2
Sarah Dandou <sarah.dandou@insa-lyon.fr>
2020-07-07 16:23:06 +0200

    Mute problems

--- tests/test_go.py
+++ tests/test_go.py
@@ -11,7 +11,7 @@ from starlette.requests import Request
 from starlette.responses import JSONResponse, HTMLResponse
 from starlette.testclient import TestClient
 
-from biota.go import GO, GOJSONViewModel, GOViewer
+from biota.go import GO, GOJSONViewModel #, GOViewer
 from manage import settings
 
 ############################################################################################
