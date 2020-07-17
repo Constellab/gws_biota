70fee64fe3b0146562b66d4978c9a0b211d28617
Djomangan Adama OUATTARA <douattara@gencovery.com>
2020-07-17 22:12:13 +0200

    bugfix: buttons in settings.json

--- public/static/index.js
+++ public/static/index.js
@@ -4,3 +4,10 @@
  * The use and distribution of this software is prohibited without the prior consent of Gencovery SAS.
  * About us: https://gencovery.com
  */
+
+if( window.gws == undefined )
+    window.gws = {}
+
+window.gws.biota = function(){
+    console.log("biota loaded")
+}
