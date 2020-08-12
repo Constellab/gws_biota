a2e50998eadf7e9191b157be9bc7f2c51d422045
Djomangan Adama OUATTARA <douattara@gencovery.com>
2020-08-12 14:00:52 +0200

    Add private _cli module; Update App class to use metaprogramming functionalities

---
+++ biota/_cli/createdb.py
@@ -0,0 +1,187 @@
+# LICENSE
+# This software is the exclusive property of Gencovery SAS. 
+# The use and distribution of this software is prohibited without the prior consent of Gencovery SAS.
+# About us: https://gencovery.com
+
+import os
+import click
+import logging
+
+#import from biota
+from gws.settings import Settings
+from biota.db.go import GO
+from biota.db.sbo import SBO
+from biota.db.bto import BTO
+from biota.db.eco import ECO
+from biota.db.chebi_ontology import ChebiOntology
+from biota.db.taxonomy import Taxonomy
+from biota.db.compound import Compound
+from biota.db.enzyme_function import EnzymeFunction
+#from biota.db.enzyme_annotation import EnzymeAnnotation
+from biota.db.reaction import Reaction
+
+#import Timer
+from timeit import default_timer
+import time
+
+def createdb(user):
+    settings = Settings.retrieve()
+
+    if user is None:
+        user = "Gencoverer"
+
+    log_dir = settings.get_log_dir()
+    if not os.path.exists(log_dir):
+        os.mkdir(log_dir)
+
+    fh = logging.FileHandler(os.path.join(log_dir,'./biota.log'))
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(" %(message)s")
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    logger.info(f"Hello {user}")
+    logger.info(f"Create tables ...")
+
+    # drop tables
+    GO.drop_table()
+    SBO.drop_table()
+    BTO.drop_table()
+    ECO.drop_table()
+    ChebiOntology.drop_table()
+    Taxonomy.drop_table()
+    Compound.drop_table()
+    EnzymeFunction.drop_table()
+    Reaction.drop_table()
+    #EnzymeAnnotation.drop_table()
+
+    # create tables
+    GO.create_table()
+    SBO.create_table()
+    BTO.create_table()
+    ECO.create_table()
+    ChebiOntology.create_table()
+    Taxonomy.create_table()
+    Compound.create_table()
+    EnzymeFunction.create_table()
+    Reaction.create_table()
+    #EnzymeAnnotation.create_table()
+    
+    logger.info("Start creating biota_db...")
+
+    files = dict(
+            go_file = "go.obo",
+            sbo_file = "sbo.obo",
+            eco_file = "eco.obo",
+            chebi_file = "./obo/chebi.obo",
+            bto_file = "bto.json",
+            pwo_file = "./pwo/pwo.obo",
+            brenda_file = "./brenda/brenda_download.txt",
+            bkms_file = "./bkms/Reactions_BKMS.csv",
+            chebi_compound_file = "./tsv/compounds.tsv",
+            chebi_chemical_data_file = "./tsv/chemical_data.tsv",
+            ncbi_node_file = "./taxdump/nodes.dmp",
+            ncbi_name_file = "./taxdump/names.dmp",
+            ncbi_division_file = "./taxdump/division.dmp",
+            ncbi_citation_file = "./taxdump/citations.dmp",
+            rhea_kegg_reaction_file = './kegg/rhea-kegg.reaction',
+            rhea_direction_file = './tsv/rhea-directions.tsv',
+            rhea2ecocyc_file = './tsv/rhea2ecocyc.tsv',
+            rhea2metacyc_file = './tsv/rhea2metacyc.tsv',
+            rhea2macie_file = './tsv/rhea2macie.tsv',
+            rhea2kegg_reaction_file = './tsv/rhea2kegg_reaction.tsv',
+            rhea2ec_file = './tsv/rhea2ec.tsv',
+            rhea2reactome_file = './tsv/rhea2reactome.tsv'
+        )
+
+    # ------------- Create GO ------------- #
+    logger.info("Step 1 | Loading go and go_ancestors...")
+    start_time = time.time()
+    go_biodata_dir = settings.get_data("biota:go_biodata_dir")
+    GO.create_go_db(go_biodata_dir, **files)
+    len_go = GO.select().count()
+    elapsed_time = time.time() - start_time
+    logger.info("... done in {:10.2f} min for #go = {}".format(elapsed_time/60, len_go))
+    
+    # ------------- Create SBO ------------- #
+    logger.info("Step 2 | Loading sbo and sbo_ancestors...")
+    start_time = time.time()
+    sbo_biodata_dir = settings.get_data("biota:sbo_biodata_dir")
+    SBO.create_sbo_db(sbo_biodata_dir, **files)
+    len_sbo = SBO.select().count()
+    elapsed_time = time.time() - start_time
+    logger.info("... done in {:10.2f} sec for #sbo= {}".format(elapsed_time, len_sbo))
+    
+    # ------------------- Create BTO ----------------- #
+    logger.info("Step 3 | Loading bto and bto_ancestors...")
+    start_time = time.time()
+    bto_biodata_dir = settings.get_data("biota:bto_biodata_dir")
+    BTO.create_bto_db(bto_biodata_dir, **files)
+    len_bto = BTO.select().count()
+    elapsed_time = time.time() - start_time
+    logger.info("... done in {:10.2f} sec for #bto = {}".format(elapsed_time, len_bto))
+    
+    # ------------------- Create ECO ----------------- #
+    logger.info("Step 4 | Loading eco and eco_ancestors...")
+    start_time = time.time()
+    eco_biodata_dir = settings.get_data("biota:eco_biodata_dir")
+    ECO.create_eco_db(eco_biodata_dir, **files)
+    len_eco = ECO.select().count()
+    elapsed_time = time.time() - start_time
+    logger.info("... done in {:10.2f} sec for #eco = {}".format(elapsed_time, len_eco))
+    
+    # ------------- Create ChebiOntology ------------- #
+    logger.info("Step 5 | Loading chebi ontology...")
+    start_time = time.time()
+    chebi_biodata_dir = settings.get_data("biota:chebi_biodata_dir")
+    ChebiOntology.create_chebi_ontology_db(chebi_biodata_dir, **files)
+    len_chebi_ontology = ChebiOntology.select().count()
+    elapsed_time = time.time() - start_time
+    logger.info("... done in {:10.2f} min for #chebi_ontology = {}".format(elapsed_time/60, len_chebi_ontology))
+
+    # ---------------- Create Taxonomy --------------- #
+    logger.info("Step 6 | Loading ncbi taxonomy...")
+    start_time = time.time()
+    ncbi_biodata_dir = settings.get_data("biota:taxonomy_biodata_dir")
+    Taxonomy.create_taxonomy_db(ncbi_biodata_dir, **files)
+    len_taxonomy = Taxonomy.select().count()
+    elapsed_time = time.time() - start_time
+    logger.info("... done in {:10.2f} min for #taxa = {}".format(elapsed_time/60, len_taxonomy))
+    
+    # ---------------- Create Compound --------------- #
+    logger.info("Step 7 | Loading chebi compounds...")
+    start_time = time.time()
+    chebi_biodata_dir = settings.get_data("biota:chebi_biodata_dir")
+    Compound.create_compound_db(chebi_biodata_dir, **files)
+    len_compound = Compound.select().count()
+    elapsed_time = time.time() - start_time
+    logger.info("... done in {:10.2f} min for #compounds = {} ".format(elapsed_time/60, len_compound))
+
+    # ------------------ Create EnzymeFunction --------------- #
+    logger.info("Step 8 | Loading brenda enzyme_functions and enzyme_btos...")
+    start_time = time.time()
+    brenda_biodata_dir = settings.get_data("biota:brenda_biodata_dir")
+    EnzymeFunction.create_enzyme_function_db(brenda_biodata_dir, **files)
+    len_enzyme = EnzymeFunction.select().count()
+    elapsed_time = time.time() - start_time
+    logger.info("... done in {:10.2f} min for #enzyme_functions = {} ".format(elapsed_time/60, len_enzyme))
+    
+    # ---------------- Create Reactions -------------- #
+    logger.info("Step 9 | Loading rhea reactions...")
+    start_time = time.time()
+    rhea_biodata_dir = settings.get_data("biota:rhea_biodata_dir")
+    Reaction.create_reaction_db(rhea_biodata_dir, **files)
+    len_rhea = Reaction.select().count()
+    elapsed_time = time.time() - start_time
+    logger.info("... done in {:10.2f} min for #rhea = {}".format(elapsed_time/60, len_rhea))
+    
+    """"
+    # ------------- Create EnzymeAnnotation ------------- #
+    logger.info("Step 10 | Loading enzyme_function annotations...")
+    start_time = time.time()
+    EnzymeAnnotation.create_annotation_db()
+    len_enzyme_annotation = EnzymeAnnotation.select().count()
+    elapsed_time = time.time() - start_time
+    logger.info("... done in {:10.2f} min for #enzyme_annotations = {}".format(elapsed_time/60, len_enzyme_annotation))
+    """
--- biota/app.py
+++ biota/app.py
@@ -11,19 +11,10 @@ from starlette.templating import Jinja2Templates
 
 from gws.settings import Settings
 from gws.prism.controller import Controller
-from gws.app import App as GWSApp
-
-from biota.db.go import GO, GOJSONStandardViewModel, GOJSONPremiumViewModel
-from biota.db.taxonomy import Taxonomy, TaxonomyJSONStandardViewModel, TaxonomyJSONPremiumViewModel
-from biota.db.compound import Compound, CompoundJSONStandardViewModel, CompoundJSONPremiumViewModel
 
 settings = Settings.retrieve()
 template_dir = settings.get_template_dir("biota")
 templates = Jinja2Templates(directory=template_dir)
-
-dict_descriptions = {
-    "GO": "The Gene Ontology: a major bioinformatics initiative to unify the representation of gene and gene product attributes across all species."
-}
 
 async def homepage(request):
     return templates.TemplateResponse('home.html', {'request': request, 'settings': settings})
@@ -35,50 +26,31 @@ async def cardpage(request):
     return templates.TemplateResponse('card.html', {'request': request, 'settings': settings})
 
 async def test_view(request):
-    view = App.get_go_example_views()
-    dict_ = App.get_go_example_views()
-    for view in dict_.keys():
-        dict_[view] = json.loads(dict_[view])
+    return templates.TemplateResponse('test_view.html', {'request': request, 'settings': settings})
 
-    response = templates.TemplateResponse('test_view.html', {'request': request, 'settings': settings, "views": dict_})
-    return response
-    #return (templates.TemplateResponse('test_view.html', {'request': request, 'settings': settings}))
-    #return templates.TemplateResponse('test_view.html', {'request': request, 'settings': settings})
+class App:
+    """
+    App class of biota application
 
-class App(GWSApp):
+    This App class will dynamically inherits the App classes this application depends on.
+    Method on_init() must be overloaded to add new routes.
+    """
+
+    routes = []
 
     @classmethod
     def on_init(cls):
+        """
+        Initializes the application. 
+        
+        This method is automatically called after by the constructor.
+        """
+        # loads base applications' routes
         super().on_init()
 
-        #biota routes
+        # adds new routes
         cls.routes.append(Route('/biota/home/', homepage) )
         cls.routes.append(Route('/biota/tables/', tablepage) )
         cls.routes.append(Route('/biota/card/{name}/{id}', cardpage) )
         cls.routes.append(Route('/biota/testviews/', test_view ) )
-    
-    def get_go_example_views():
-        dict_views = {}
-        list_views = []
-        # ------------------ First example ------------------ #
-        go1 = GO.get(GO.go_id == 'GO:0000006')
-        go1_premium_view_model = GOJSONStandardViewModel(go1)
-        view1 = go1_premium_view_model.render()
-        list_views.append(view1)
-
-        # ------------------ Second example ------------------ #
-        go2 = GO.get(GO.go_id == 'GO:0000011')
-        go2_premium_view_model = GOJSONPremiumViewModel(go2)
-        view2 = go2_premium_view_model.render()
-        list_views.append(view2)
-
-        # ------------------ Third example ------------------ #
-        go3 = GO.get(GO.go_id == 'GO:0000012')
-        go3_premium_view_model = GOJSONPremiumViewModel(go3)
-        view3 = go3_premium_view_model.render()
-        dict_views = {1: view1.replace('\'', '\"'), 2: view2.replace("\'", ""), 3: view3.replace("\'", "")}
-        list_views.append(view1)
-
-        return dict_views
-
 
