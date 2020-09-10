554046a52d7d6e45f663ee8c84f9893851d40d53
Djomangan Adama OUATTARA <douattara@gencovery.com>
2020-09-10 21:48:48 +0200

    Install dependencies from env.sh

--- env.sh
+++ env.sh
@@ -10,11 +10,9 @@ python3 -m pip install virtualenv
 python3 -m virtualenv .venv --python=python3
 
 # activate vitual env
-. ./.venv/bin/activate
+source ./.venv/bin/activate
 
-# prism requirement file
-for req in "$@"
-do
-  python3 -m pip install -r "$req"
-done
+# install dependencies
+python3 -m pip install -r ../gws-py/requirements.txt
+python3 -m pip install -r requirements.txt
 
