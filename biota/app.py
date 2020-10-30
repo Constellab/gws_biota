14801aedfa3309a4c65bc961cd790eb1abe83c1b
Djomangan Adama OUATTARA <douattara@gencovery.com>
2020-10-30 23:06:27 +0100

    Bug fix: Do not mount subapps on startup

--- biota/app.py
+++ biota/app.py
@@ -3,5 +3,5 @@
 # The use and distribution of this software is prohibited without the prior consent of Gencovery SAS.
 # About us: https://gencovery.com
 
-async def page_index(request):
+async def index_page(request):
     return None
