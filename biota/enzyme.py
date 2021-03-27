4ae66ba9dd8a92d64761f90778fbcc1246be4fb5
Djomangan Adama OUATTARA <douattara@gencovery.com>
2021-03-27 22:23:31 +0000

    Minor changes

--- biota/enzyme.py
+++ biota/enzyme.py
@@ -261,6 +261,17 @@ class Enzo(Base):
         return ",".join([ sn.capitalize() for sn in self.data.get("SN", ['']) ])
         
 class DeprecatedEnzyme(Base):
+    """
+    This class represents depreacted EC numbers of enzymes. 
+    A deprecated enzyme is an enzyme of which the EC number has changed at least once. 
+    The old EC number is therefore tagged as deprecated and is not valid anymore.
+
+    :property ec_number: The deprecated ec number
+    :type ec_number: str
+    :property new_ec_number: The new ec number
+    :type new_ec_number: str
+    """
+    
     ec_number = CharField(null=True, index=True) 
     new_ec_number = CharField(null=True, index=True) 
     _table_name = 'biota_deprecated_enzymes'
@@ -270,19 +281,22 @@ class DeprecatedEnzyme(Base):
         return self.data["reason"]
     
     def select_new_enzymes(self):
-        try:
-            return Enzyme.select().where(Enzyme.ec_number == self.new_ec_number)
-        except:
-            return None
-        
-    #@property
-    #def new_enzyme(self):
-    #    try:
-    #        return Enzyme.get(Enzyme.ec_number == self.new_ec_number)
-    #    except:
-    #        return None
-        
-        
+        Q = []
+        if self.new_ec_number:
+            tmp_Q = Enzyme.select().where(Enzyme.ec_number == self.new_ec_number)        
+            if tmp_Q:
+                for e in tmp_Q:
+                    Q.append(e)
+            else:
+                Q = []
+                # if the new enzyme if also deprecated, we follow the deprecation chain
+                deprec_Q = DeprecatedEnzyme.select().where(DeprecatedEnzyme.ec_number == self.new_ec_number)
+                for deprec in deprec_Q:
+                    for e in deprec.select_new_enzymes():
+                        Q.append(e)
+                    
+        return Q
+
 class Enzyme(Base):
     """
     This class represents enzymes extracted from open databases.
