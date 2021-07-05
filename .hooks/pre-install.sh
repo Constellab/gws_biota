9f9df6d86fac793bf80e35a0057672e51cc98891
Djomangan Adama OUATTARA <douattara@gencovery.com>
2021-07-05 20:58:03 +0000

    [db] upload db with pre-install hook

--- .hooks/pre-install.sh
+++ .hooks/pre-install.sh
@@ -5,3 +5,13 @@
 
 
 # Pre-installation script executed before server installation
+
+data_dir="/data/sqlite3"
+sqlite3_file="${data_dir}/biota.sqlite3"
+
+if [ ! -f "$sqlite3_file" ]; then
+    curl https://share.gencovery.com/s/Eo34Y8sxgqSdSMz/download -o "${sqlite3_file}.zip"
+    cd "$data_dir"
+    unzip -p "${sqlite3_file}.zip" > "${sqlite3_file}"
+    rm -f "${sqlite3_file}.zip"
+fi
