1f0da5ae3fd50f84d53365a0b7cc1c5447f6b0e3
Djomangan Adama OUATTARA <douattara@gencovery.com>
2021-07-06 13:28:46 +0000

    [hook] create db dir before uploading biota db

--- .hooks/pre-install.sh
+++ .hooks/pre-install.sh
@@ -10,6 +10,10 @@ data_dir="/data/sqlite3"
 sqlite3_file="${data_dir}/biota.sqlite3"
 
 if [ ! -f "$sqlite3_file" ]; then
+    if [ ! -d "$data_dir" ]; then
+        mkdir -p $data_dir
+    fi
+
     curl https://share.gencovery.com/s/Eo34Y8sxgqSdSMz/download -o "${sqlite3_file}.zip"
     unzip -p "${sqlite3_file}.zip" > "${sqlite3_file}"
     rm -f "${sqlite3_file}.zip"
