8a6feee1e4ae4546b3f1c930fdc149736fa73656
Djomangan OUATTARA <douattara@gencovery.com>
2021-11-26 14:48:52 +0000

    (refactor) minor refactoring

--- src/gws_biota/db/db_service.py
+++ src/gws_biota/db/db_service.py
@@ -1,13 +1,13 @@
 # LICENSE
-# This software is the exclusive property of Gencovery SAS. 
+# This software is the exclusive property of Gencovery SAS.
 # The use and distribution of this software is prohibited without the prior consent of Gencovery SAS.
 # About us: https://gencovery.com
 
 import os
 
-from gws_core import (Settings, Protocol, Experiment, Study, Queue, Job, Requests, 
+from gws_core import (Settings, Protocol, Experiment, Study, Queue, Job, Requests,
                         BaseService, MySQLBase, MySQLService, BadRequestException,
-                        Experiment, TaskService, ExperimentService, UserService, 
+                        Experiment, TaskService, ExperimentService, UserService,
                         CurrentUserService, QueueService, TaskModel)
 from .db_creator import DbCreator
 
@@ -21,16 +21,14 @@ class DbService(BaseService):
 
         if not user:
             user = UserService.get_sysuser()
-            user.is_http_authenticated = True
-            user.is_console_authenticated = True
             user.save()
             CurrentUserService.set_current_user(user)
 
-        
+
         db_creator_model: TaskModel = TaskService.create_task_model_from_type(task_type=DbCreator)
         experiment: Experiment =  ExperimentService.create_experiment_from_task_model(
             task_model=db_creator_model
-        ) 
+        )
         experiment.save()
         experiment.set_title("Creation of biota database")
         try:
@@ -41,7 +39,7 @@ class DbService(BaseService):
 
         experiment.refresh()
         return experiment
-        
+
     @classmethod
     def dump_biota_db(cls) -> bool:
         MySQLService.dump_db("biota", force=True, wait=False)
