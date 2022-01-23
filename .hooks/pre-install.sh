c536f289cb2ae01c753aca2081c41551239b2bc1
Djomangan OUATTARA <douattara@gencovery.com>
2022-01-23 16:43:12 +0000

    (fix) freeze packages versions

--- .hooks/pre-install.sh
+++ .hooks/pre-install.sh
@@ -5,20 +5,3 @@
 
 
 # Pre-installation script executed before server installation
-
-# apt-get -y install jq
-# data_dir="/data/gws_biota/sqlite3"
-# sqlite3_file="${data_dir}/gws_biota.sqlite3"
-# FILE_DIR=$(dirname "$0")
-# sqlite3_db_url=`jq '.variables."gws_biota:sqlite3db_url"' ${FILE_DIR}/../settings.json | sed -e 's/^"//' -e 's/"$//'`
-
-# echo $sqlite3_db_url
-# if [ ! -f "$sqlite3_file" ]; then
-#     if [ ! -d "$data_dir" ]; then
-#         mkdir -p $data_dir
-#     fi
-
-#     curl $sqlite3_db_url -o "${sqlite3_file}.zip"
-#     unzip -p "${sqlite3_file}.zip" > "${sqlite3_file}"
-#     rm -f "${sqlite3_file}.zip"
-# fi
