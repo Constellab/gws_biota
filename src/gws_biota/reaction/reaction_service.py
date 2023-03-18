2fb940ebb9b6aba00fd8d554232a4c2c50f9325f
Djomangan OUATTARA <douattara@gencovery.com>
2023-03-18 21:15:30 +0000

    (fix) uniquify data in fulltext indexes of Reaction

--- src/gws_biota/reaction/reaction_service.py
+++ src/gws_biota/reaction/reaction_service.py
@@ -162,13 +162,9 @@ class ReactionService(BaseService):
 
             # set ft_names
             ft_names = [react.rhea_id.replace(":", ""), *ft_names]
-            react.ft_names = ";".join(ft_names)
-
-            # set ft_tax_ids
-            react.ft_tax_ids = ";".join(ft_tax_ids)
-
-            # set ft_ec_numbers
-            react.ft_ec_numbers = ";".join(ft_ec_numbers)
+            react.ft_names = ";".join(list(set(ft_names)))
+            react.ft_tax_ids = ";".join(list(set(ft_tax_ids)))
+            react.ft_ec_numbers = ";".join(list(set(ft_ec_numbers)))
 
         return vals
     # -- U --
