083f83af229b2693fe20fd5df41b9d8514391404
Maëva <mbeugin@gencovery.com>
2026-02-09 16:04:42 +0000

    (fix) fix import

--- src/gws_biota/_helper/quickgo.py
+++ src/gws_biota/_helper/quickgo.py
@@ -1,7 +1,5 @@
-
-
 import requests
-from gws.settings import Settings
+from gws_core import Settings
 
 
 class QuickGOAnnotation:
@@ -11,6 +9,7 @@ class QuickGOAnnotation:
     Results are return in a python readable and exploitable format
 
     """
+
     @classmethod
     def get_tsv_file_from_uniprot_id(cls, uniprot_id):
         """
@@ -34,20 +33,20 @@ class QuickGOAnnotation:
         URL = settings.get_variable("biota:quickgo_api_url")
         try:
             requestURL = URL + str(uniprot_id)
-            r = requests.get(requestURL, headers={ "Accept" : "text/tsv"})
+            r = requests.get(requestURL, headers={"Accept": "text/tsv"})
 
             if not r.ok:
                 r.raise_for_status()
             else:
                 responseBody = r.text
-                text = responseBody.split('\n')
-                text = text[0:len(text)-1]
+                text = responseBody.split("\n")
+                text = text[0 : len(text) - 1]
                 list_return = cls.__parse_tsv_from_file(text)
-            return(list_return)
+            return list_return
 
         except:
             pass
-            #print('Can not find the uniprot id ' + uniprot_id + ' on QuickGO')
+            # print('Can not find the uniprot id ' + uniprot_id + ' on QuickGO')
 
     @classmethod
     def __parse_tsv_from_file(cls, list_text) -> list:
@@ -69,19 +68,19 @@ class QuickGOAnnotation:
         list_annotations = []
         line_count = 0
         for i in range(0, len(list_text)):
-            if(line_count < 1):
-                infos_table = list_text[0].split('\t')
-                infos_table[len(infos_table)-1] = infos_table[len(infos_table)-1].replace('\n', '')
+            if line_count < 1:
+                infos_table = list_text[0].split("\t")
+                infos_table[len(infos_table) - 1] = infos_table[len(infos_table) - 1].replace(
+                    "\n", ""
+                )
                 for i in range(0, len(infos_table)):
                     infos_table[i] = infos_table[i].lower()
                 line_count += 1
             else:
                 dict_annotation = {}
                 for j in range(0, len(infos_table)):
-                    list_info = list_text[i].split('\t')
-                    list_info[len(list_info)-1] = list_info[len(list_info)-1].replace('\n', '')
+                    list_info = list_text[i].split("\t")
+                    list_info[len(list_info) - 1] = list_info[len(list_info) - 1].replace("\n", "")
                     dict_annotation[infos_table[j]] = list_info[j]
                 list_annotations.append(dict_annotation)
-        return(list_annotations)
-
-
+        return list_annotations
